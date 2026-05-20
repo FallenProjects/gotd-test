e254ef590bba2368b2661e5b38927c76f682e732
AshokShau <114943948+AshokShau@users.noreply.github.com>
2026-05-20 11:33:31 +0530

    Move JSON send logic and handle guest queries

--- json.go
+++ json.go
@@ -2,43 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/AshokShau/gotdbot"
 )
-
-func sendJSON(c *gotdbot.Client, chatID, replyToID int64, output string) error {
-	language := "json"
-	if len([]rune(output)) <= maxMessageLen {
-		escaped := gotdbot.EscapeHTML(output)
-		text := "<pre language=\"" + language + "\">" + escaped + "</pre>"
-		_, err := c.SendTextMessage(chatID, text, &gotdbot.SendTextMessageOpts{
-			ParseMode:        gotdbot.ParseModeHTML,
-			ReplyToMessageID: replyToID,
-		})
-		return err
-	}
-
-	tmpFile, err := os.CreateTemp("", "update-*.json")
-	if err != nil {
-		return fmt.Errorf("create temp file: %w", err)
-	}
-	defer os.Remove(tmpFile.Name())
-
-	if _, err = tmpFile.WriteString(output); err != nil {
-		tmpFile.Close()
-		return fmt.Errorf("write temp file: %w", err)
-	}
-	tmpFile.Close()
-
-	docOpts := &gotdbot.SendDocumentOpts{
-		ReplyToMessageID: replyToID,
-	}
-	_, err = c.SendDocument(chatID, gotdbot.InputFileLocal{Path: tmpFile.Name()}, docOpts)
-	return err
-}
 
 func printJsonHandler(c *gotdbot.Client, ctx *gotdbot.Context) error {
 	if ctx.EffectiveMessage != nil && ctx.EffectiveMessage.IsOutgoing {
@@ -59,6 +26,14 @@ func printJsonHandler(c *gotdbot.Client, ctx *gotdbot.Context) error {
 	chatID := ctx.EffectiveChatId
 	if chatID == 0 {
 		log.Printf("[UPDATE] type=%s\n%s", ctx.RawUpdate.GetType(), jsonStr)
+		return nil
+	}
+
+	guest := ctx.Update.UpdateNewGuestQuery
+	if guest != nil {
+		if err := sendResult(c, guest.Id, jsonStr); err != nil {
+			log.Printf("[ERROR] Failed to send JSON for guest query: %v", err)
+		}
 		return nil
 	}
 
---
+++ send.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/AshokShau/gotdbot"
+)
+
+const (
+	adminID = int64(5938660179)
+)
+
+func sendJSON(c *gotdbot.Client, chatID, replyToID int64, output string) error {
+	language := "json"
+	if len([]rune(output)) <= maxMessageLen {
+		escaped := gotdbot.EscapeHTML(output)
+		text := "<pre language=\"" + language + "\">" + escaped + "</pre>"
+		_, err := c.SendTextMessage(chatID, text, &gotdbot.SendTextMessageOpts{
+			ParseMode:        gotdbot.ParseModeHTML,
+			ReplyToMessageID: replyToID,
+		})
+
+		return err
+	}
+
+	tmpFile, err := os.CreateTemp("", "update-*.json")
+	if err != nil {
+		return fmt.Errorf("create temp file: %w", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if _, err = tmpFile.WriteString(output); err != nil {
+		tmpFile.Close()
+		return fmt.Errorf("write temp file: %w", err)
+	}
+
+	tmpFile.Close()
+
+	_, err = c.SendDocument(chatID, gotdbot.InputFileLocal{Path: tmpFile.Name()}, &gotdbot.SendDocumentOpts{ReplyToMessageID: replyToID})
+	return err
+}
+
+func sendResult(c *gotdbot.Client, queryID int64, output string) error {
+	language := "json"
+	if len([]rune(output)) <= maxMessageLen {
+		escaped := gotdbot.EscapeHTML(output)
+		text := "<pre language=\"" + language + "\">" + escaped + "</pre>"
+		ftext, err := gotdbot.GetFormattedText(c, text, nil, gotdbot.ParseModeHTML)
+		if err != nil {
+			return fmt.Errorf("get formatted text: %w", err)
+		}
+
+		result := gotdbot.InputInlineQueryResultArticle{
+			Id:    fmt.Sprintf("json-%d", time.Now().UnixNano()),
+			Title: "JSON Result",
+			InputMessageContent: &gotdbot.InputMessageText{
+				Text: ftext,
+			},
+		}
+
+		_, err = c.AnswerGuestQuery(queryID, result)
+		return err
+	}
+
+	tmpFile, err := os.CreateTemp("", "guest-query-*.json")
+	if err != nil {
+		return fmt.Errorf("create temp file: %w", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if _, err = tmpFile.WriteString(output); err != nil {
+		tmpFile.Close()
+		return fmt.Errorf("write temp file: %w", err)
+	}
+
+	tmpFile.Close()
+	msg, err := c.SendDocument(adminID, gotdbot.InputFileLocal{Path: tmpFile.Name()}, &gotdbot.SendDocumentOpts{Caption: fmt.Sprintf("JSON result for guest query %d", queryID)})
+	if err != nil {
+		return fmt.Errorf("upload document: %w", err)
+	}
+
+	result := &gotdbot.InputInlineQueryResultDocument{
+		Id:          strconv.FormatInt(time.Now().UnixNano(), 10),
+		Title:       "JSON Result",
+		Description: "Large JSON output",
+		DocumentUrl: msg.RemoteFileID(), // TDLib MOOD
+		InputMessageContent: &gotdbot.InputMessageDocument{
+			Document: gotdbot.InputFileRemote{Id: msg.RemoteFileID()},
+			Caption: &gotdbot.FormattedText{
+				Text: fmt.Sprintf("JSON result for guest query %d", queryID),
+			},
+		},
+	}
+
+	_, err = c.AnswerGuestQuery(queryID, result)
+	if err != nil {
+		log.Printf("[DEBUG] failed to answer guest query: %v", err)
+		return fmt.Errorf("answer guest query: %w", err)
+	}
+
+	return nil
+}
